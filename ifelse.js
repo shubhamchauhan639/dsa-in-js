3e13e59f66858f38ee97c9fa28b5239a2365c426
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-09-05 21:13:42 +0530

    ifelse discount

--- ifelse.js
+++ ifelse.js
@@ -1,8 +1,24 @@
-let ans = 25
-if (ans >= 18) {
-    console.log("you are alegible to vote");
+// let ans = 25
+// if (ans >= 18) {
+//     console.log("you are alegible to vote");
     
-} else {
-console.log("you are not eligible");
+// } else {
+// console.log("you are not eligible");
+    
+// }
+
+let amount = 9000
+if (amount > 0 && amount<=5000) {
+    console.log(amount);
+     
+} else if (amount>5000&&amount<=7000){
+    console.log(amount-Math.floor((5*amount)/100));
+} else if (amount>7000&&amount<=9000){
+    console.log(amount-Math.floor((10*amount)/100));
+} else if (amount>9000){
+    console.log(amount-Math.floor((20*amount)/100));
+}
+else{
+    console.log("wrong input");
     
 }
