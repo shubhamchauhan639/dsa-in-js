480b3e2ac8a74dfe107e43fd473f95757b9938ca
shubhamchauhan639 <shubhamchauhan12196@gmail.com>
2025-09-06 12:38:23 +0530

    electricity bill unit system using if else

---
+++ bill.js
@@ -0,0 +1,18 @@
+let unit = 350;
+let amount = 0
+if (unit>400) {
+    amount = (unit-400)*13;
+    unit = 400
+}
+if (unit>200&&unit<=400) {
+    amount += (unit-200)*8;
+    unit = 200
+    
+}
+if (unit>100&&unit<=200) {
+    amount += (unit-100)*6;
+    unit = 100
+    
+}
+amount += unit * 4;
+console.log(amount);
